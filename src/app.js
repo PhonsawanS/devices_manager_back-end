39799abaa49d6db1c1382ae86ff0bbe1b2fac6f7
PhonsawanS <phonsawan.suarab@gmail.com>
2025-05-11 18:52:00 +0700

    edit cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const routes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://devices-manager-font-end-coz8.vercel.app"
+}));
+
 app.use(express.json());
 app.use('/api', routes);
 
